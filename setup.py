8f0513c59edbcb11b16428287e1473b712086132
Michael Cooper <mic159@gmail.com>
2015-06-20 18:08:42 +1000

    Lisence

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='benbox-slicer',
     version='1.0.0',
     packages=find_packages(),
+    license='MIT',
+    author='mic159',
+    description='Slice PNGs into Benbox flavour GCODE',
 
     entry_points={
         'console_scripts': [
